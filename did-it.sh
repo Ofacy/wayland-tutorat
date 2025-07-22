20abc140957b1b821167ca35ad9ba8332010de2a
Raphael <rparodi@student.42.fr>
2025-07-22 17:38:13 +0200

    style(script/didit): removing the leave command

--- did-it.sh
+++ did-it.sh
@@ -17,6 +17,3 @@ printf "\n";
 
 printf "Disclaimer: this is done for \e[34;4mpedalogical\e[0;0m purpose only\nNothing else was changed on your session this is just a display\n\n\n\033[0m\n";
 printf "Disclaimer: Ce message est a but \e[34;4mpedalogique\e[0;0m uniquement\nRien n'as ete changer sur votre session, c'est juste de l'affichage.\n\n\n\033[0m\n\n";
-
-printf "To leave: ctrl + c / ctrl + d\n";
-printf "Pour quitter: ctrl + c / ctrl + d\n";
