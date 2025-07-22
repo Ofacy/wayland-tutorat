568b2d5793d822dc77217ae3d35f0f3e32576d23
Raphael <rparodi@student.42.fr>
2025-07-22 17:38:45 +0200

    feat(script/loveu): now in french too

--- loveu.sh
+++ loveu.sh
@@ -7,4 +7,5 @@ printf ' / / / /_/ / /_/ /_/ / /     / /_/  __/ /_/ / / / / / /  / / /_/ / |/ / 
 printf '/_/  \__,_/\__/\____/_/      \__/\___/\__,_/_/ /_/ /_/  /_/\____/|___/\___/   \__,_/  \n'
 printf '\n\n'
 
+printf "Disclaimer: Ce message est a but \e[34;4mpedalogique\e[0;0m uniquement\nRien n'as ete changer sur votre session, c'est juste de l'affichage.\n\n\n\033[0m\n\n";
 printf "Disclaimer: this is done for \e[34;4mpedalogical\e[0;0m purpose only\nNothing else was changed on your session this is just a display\n\n\n\033[0m\n";
