3a08edd2792caf6d5cbe2bfbe9b78061aa26b567
Raphael <rparodi@student.42.fr>
2025-07-22 17:12:54 +0200

    feat(scripts/loveu): adding the mac support (for testing issues)

--- loveu.sh
+++ loveu.sh
@@ -1,10 +1,10 @@
-clear;echo -ne "\n\nMy login is\033[33m $USER ! \033[0m\n\n";
+clear;printf "\n\nMy login is\033[33m $USER ! \033[0m\n\n";
 
-echo '  ______      __                __                          __                        '
-echo ' /_  __/_  __/ /_____  _____   / /____  ____ _____ ___     / /___ _   _____     __  __'
-echo '  / / / / / / __/ __ \/ ___/  / __/ _ \/ __ `/ __ `__ \   / / __ \ | / / _ \   / / / /'
-echo ' / / / /_/ / /_/ /_/ / /     / /_/  __/ /_/ / / / / / /  / / /_/ / |/ /  __/  / /_/ / '
-echo '/_/  \__,_/\__/\____/_/      \__/\___/\__,_/_/ /_/ /_/  /_/\____/|___/\___/   \__,_/  '
-echo -e "\n";
+printf '  ______      __                __                          __                        \n'
+printf ' /_  __/_  __/ /_____  _____   / /____  ____ _____ ___     / /___ _   _____     __  __\n'
+printf '  / / / / / / __/ __ \/ ___/  / __/ _ \/ __ `/ __ `__ \   / / __ \ | / / _ \   / / / /\n'
+printf ' / / / /_/ / /_/ /_/ / /     / /_/  __/ /_/ / / / / / /  / / /_/ / |/ /  __/  / /_/ / \n'
+printf '/_/  \__,_/\__/\____/_/      \__/\___/\__,_/_/ /_/ /_/  /_/\____/|___/\___/   \__,_/  \n'
+printf '\n\n'
 
-echo -ne "Disclaimer : this is done for \e[34;4mpedalogical\e[0;0m purpose only\nNothing else was changed on your session this is just a display\n\n\n\033[0m\n";
+printf "Disclaimer: this is done for \e[34;4mpedalogical\e[0;0m purpose only\nNothing else was changed on your session this is just a display\n\n\n\033[0m\n";
